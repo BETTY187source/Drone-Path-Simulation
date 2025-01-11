8abb33c1b1b6100f1adc72f66f5d2629e0247f49
BETTY BENIMANA <bbenimanabetty@gmail.com>
2025-01-11 23:41:06 +0000

    Update is_palindrome.py

--- is_palindrome.py
+++ is_palindrome.py
@@ -1,6 +1,7 @@
 '''Write a function is_palindrome() that checks whether a given string is a palindrome (i.e., it reads the same forwards and backwards). 
 The function should return True if the string is a palindrome and False otherwise. 
-Test the function with the string "racecar"stubborn.attack()'''
+Test the function with the string "racecar"stubborn.attack()
+'''
 
 def is_palindrome(s):
     if s == s[::-1]:
